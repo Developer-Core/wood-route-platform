a55116e3bfdbe4e94e7579875ccf956daa92fefe
psulca <psulcasanchez@gmail.com>
2026-06-09 21:52:42 -0500

    feat: add order service contracts to sales application

---
+++ DeveloperCore.WoodRoute.Platform/Sales/Application/CommandServices/IOrderCommandService.cs
@@ -0,0 +1,36 @@
+using DeveloperCore.WoodRoute.Platform.Sales.Domain.Model.Aggregates;
+using DeveloperCore.WoodRoute.Platform.Sales.Domain.Model.Commands;
+using DeveloperCore.WoodRoute.Platform.Shared.Application.Model;
+
+namespace DeveloperCore.WoodRoute.Platform.Sales.Application.CommandServices;
+
+/// <summary>
+///     Order command service contract.
+/// </summary>
+public interface IOrderCommandService
+{
+    /// <summary>
+    ///     Handles the create order command.
+    /// </summary>
+    Task<Result<Order>> Handle(CreateOrderCommand command, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    ///     Handles the modify order command.
+    /// </summary>
+    Task<Result<Order>> Handle(ModifyOrderCommand command, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    ///     Handles the cancel order command.
+    /// </summary>
+    Task<Result<Order>> Handle(CancelOrderCommand command, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    ///     Handles the accept order command.
+    /// </summary>
+    Task<Result<Order>> Handle(AcceptOrderCommand command, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    ///     Handles the reject order command.
+    /// </summary>
+    Task<Result<Order>> Handle(RejectOrderCommand command, CancellationToken cancellationToken = default);
+}
