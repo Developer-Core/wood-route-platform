830e3cdcdf071cef37ab735312fb229fa9624d70
psulca <psulcasanchez@gmail.com>
2026-06-09 19:54:03 -0500

    feat: add quote status value object to sales domain

---
+++ DeveloperCore.WoodRoute.Platform/Sales/Domain/Model/ValueObjects/EQuoteStatus.cs
@@ -0,0 +1,11 @@
+namespace DeveloperCore.WoodRoute.Platform.Sales.Domain.Model.ValueObjects;
+
+/// <summary>
+///     Represents the status of a quote proposed for an order.
+/// </summary>
+public enum EQuoteStatus
+{
+    Proposed,
+    Accepted,
+    Rejected
+}
