0340987f7308b7127bda289c9f2f4fd05810d7e3
psulca <psulcasanchez@gmail.com>
2026-06-09 19:54:03 -0500

    feat: add payment type value object to sales domain

---
+++ DeveloperCore.WoodRoute.Platform/Sales/Domain/Model/ValueObjects/EPaymentType.cs
@@ -0,0 +1,10 @@
+namespace DeveloperCore.WoodRoute.Platform.Sales.Domain.Model.ValueObjects;
+
+/// <summary>
+///     Represents the type of payment registered for an order.
+/// </summary>
+public enum EPaymentType
+{
+    Deposit,
+    FinalPayment
+}
