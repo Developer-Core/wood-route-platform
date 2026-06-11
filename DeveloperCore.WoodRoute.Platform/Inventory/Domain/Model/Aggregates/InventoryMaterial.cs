6e2cc2b05c88f2f53c158c8b7bf8b74f6f224936
Ander-U <u202120836@upc.edu.pe>
2026-06-11 18:59:20 -0500

    feat(inventory): add inventory material aggregate

---
+++ DeveloperCore.WoodRoute.Platform/Inventory/Domain/Model/Aggregates/InventoryMaterial.cs
@@ -0,0 +1,39 @@
+﻿using DeveloperCore.WoodRoute.Platform.Inventory.Domain.Model.Commands;
+using DeveloperCore.WoodRoute.Platform.Shared.Domain.Model.Entities;
+
+namespace DeveloperCore.WoodRoute.Platform.Inventory.Domain.Model.Aggregates;
+
+/// <summary>
+///     Inventory Material Aggregate Root
+/// </summary>
+
+public class InventoryMaterial : IAuditableEntity
+{
+    private InventoryMaterial()
+    {
+        MaterialType = string.Empty;
+        Unit = string.Empty;
+    }
+
+    public InventoryMaterial(CreateInventoryMaterialCommand command)
+    {
+        MaterialType = command.MaterialType;
+        Quantity = command.Quantity;
+        Unit = command.Unit;
+        MinStock = command.MinStock;
+    }
+
+    public int Id { get; }
+
+    public string MaterialType { get; private set; }
+
+    public decimal Quantity { get; private set; }
+
+    public string Unit { get; private set; }
+
+    public decimal MinStock { get; private set; }
+
+    public DateTimeOffset? CreatedAt { get; set; }
+
+    public DateTimeOffset? UpdatedAt { get; set; }
+}
