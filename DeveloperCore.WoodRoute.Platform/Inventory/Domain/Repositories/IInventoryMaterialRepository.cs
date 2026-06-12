c11cc399f25aa938a1aa3804629917e394d9f321
Ander-U <u202120836@upc.edu.pe>
2026-06-11 20:25:58 -0500

    feat(inventory): add inventory material repository contract

---
+++ DeveloperCore.WoodRoute.Platform/Inventory/Domain/Repositories/IInventoryMaterialRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace DeveloperCore.WoodRoute.Platform.Inventory.Domain.Repositories;
+
+using Model.Aggregates;
+
+public interface IInventoryMaterialRepository
+{
+    Task<InventoryMaterial?> FindByIdAsync(int id);
+    Task<IEnumerable<InventoryMaterial>> ListAsync();
+    Task AddAsync(InventoryMaterial material);
+    void Update(InventoryMaterial material);
+    void Remove(InventoryMaterial material);
+}
