69edefc94d1a9e28e8da96b6c1b7a78c74858550
Ander-U <u202120836@upc.edu.pe>
2026-06-30 17:16:02 -0500

    feat(inventory): add queries

---
+++ DeveloperCore.WoodRoute.Platform/Inventory/Domain/Model/Queries/GetAllInventoryMaterialsQuery.cs
@@ -0,0 +1,6 @@
+﻿namespace DeveloperCore.WoodRoute.Platform.Inventory.Domain.Model.Queries;
+
+/// <summary>
+///     Query to retrieve all inventory materials.
+/// </summary>
+public record GetAllInventoryMaterialsQuery;
